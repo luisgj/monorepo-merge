d7603130dc3d4c9b663ef94244af3c5edf9c22ab
luisgj <guayabas@hey.com>
2020-12-28 00:20:45 -0600

    add comment, octokit as param

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import { groupLabeledPullRequests, mergeBranches } from './src/lib'
+import { getInput } from '@actions/core';
+import { getOctokit } from '@actions/github';
 
 /**
  * main
  * @description Fetches all PRs from repo with target label and merge each one to a temp branch.
  */
 async function main() {
-    const branches = await groupLabeledPullRequests();
-    mergeBranches(branches);
+    const token = getInput('repo-token');
+    const octokit = getOctokit(token);
+    const branches = await groupLabeledPullRequests(octokit);
+    mergeBranches(octokit, branches);
 }
 main();
