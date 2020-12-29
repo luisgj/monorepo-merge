440f048efa165de8b50a29eef527d84e6824f12e
luisgj <guayabas@hey.com>
2020-12-28 21:42:49 -0600

    fix ref in cleanup function

--- src/lib.js
+++ src/lib.js
@@ -173,7 +173,7 @@ const cleanup = async function(octokit, tempBranch) {
         await octokit.request('DELETE /repos/{owner}/{repo}/git/refs/{ref}', {
             owner: context.repo.owner,
             repo: context.repo.repo,
-            ref: `refs/head/${tempBranch}`
+            ref: `refs/heads/${tempBranch}`
         });
     } catch(e) {
         console.log('Error deleting temp branch.')
